9131ce825f03c7a2cd618a7c86af4b303d4afa2d
Zakaria Chebti <zakaria@chipta.com>
2020-10-15 17:51:32 +0200

    Afronding

--- app/src/main/java/com/example/madlevel2task1/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2task1/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.madlevel2task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.madlevel2task1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val places = arrayListOf<Place>()
+    private val placeAdapter = PlaceAdapter(places)
 
     private lateinit var binding: ActivityMainBinding
 
@@ -12,5 +16,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.rvPlaces.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        binding.rvPlaces.adapter = placeAdapter
+
+        for (i in Place.PLACE_NAMES.indices) {
+            places.add(Place(Place.PLACE_NAMES[i],        Place.PLACE_RES_DRAWABLE_IDS[i]))
+
+        }
+        placeAdapter.notifyDataSetChanged()
     }
 }
